c21fd58e029947e30ed1547c9c339cda960f684d
Lord Codelrond <codelrond@protonmail.com>
2021-12-26 22:00:13 +0100

    wip added full page login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { ChakraProvider, Container, Center, Input } from "@chakra-ui/react";
+import { ChakraProvider, Container, Center, Input, Text, HStack, VStack, Box } from "@chakra-ui/react";
 import Terminal from './Terminal';
 import PouchDB from 'pouchdb-browser';
 import { Provider } from 'use-pouchdb';
-import { HStack, VStack } from '@chakra-ui/react';
+//import { HStack, VStack } from '@chakra-ui/react';
 import ManageYaml from './components/ManageYaml';
 import WorkspaceList from './components/WorkspaceList';
 import {RecoilRoot, useRecoilState, useRecoilValue} from 'recoil';
@@ -40,17 +40,30 @@ function Password(){
   // eslint-disable-next-line no-unused-vars
   const [password, setPassword] = useRecoilState(passwordAtom);
   return (
-    <Center bg='tomato' h='100px' color='white'>
-      <Input onKeyPress={e=> {
-          if (e.key === 'Enter') {
-            setPassword(e.value);
-          }
-      }}/>
-    </Center>
+    <Box position="relative">
+      <Box
+        height="100vh"
+        width="100%"
+        position="absolute"
+        top="0"
+        left="0"
+        transform="translateY(-50%, -50%)"
+      >
+        <Center bg='tomato' h='100%' w='100%' color='white'>
+          <Text>Icarus</Text>
+          <Input onKeyPress={e=> {
+              if (e.key === 'Enter') {
+                setPassword(e.value);
+              }
+          }}/>
+        </Center>
+      </Box>
+    </Box>
   )
 }
 
 function MyPouchProvider({password, children}){
+  console.log('entramos en my pouch provider')
   const localLog = new PouchDB('localLog');
   const remoteLog = new PouchDB(`http://admin:${password}@localhost:5984/icarus_log`);
 
@@ -87,24 +100,33 @@ function MyPouchProvider({password, children}){
   )
 }
 
-function App() {  
+function InnerApp(){
   const password = useRecoilValue(passwordAtom);
+  return (
+    <div>
+      {!password ? <Password /> : (
+      <MyPouchProvider password={password}>
+        <Container maxW="80rem" centerContent>
+          <HStack>
+            <WorkspaceList />
+            <VStack style={ {maxWidth: '500px'} }>
+              <ManageYaml workspace="gorilla" />
+              <Terminal />          
+            </VStack>            
+          </HStack>
+        </Container>
+      </MyPouchProvider>
+    )}          
+    </div>
+  )
+}
+
+function App() {  
+  
   return (
       <ChakraProvider>
         <RecoilRoot>
-          {!password ? <Password /> : (
-            <MyPouchProvider password={password}>
-              <Container maxW="80rem" centerContent>
-                <HStack>
-                  <WorkspaceList />
-                  <VStack style={ {maxWidth: '500px'} }>
-                    <ManageYaml workspace="gorilla" />
-                    <Terminal />          
-                  </VStack>            
-                </HStack>
-              </Container>
-            </MyPouchProvider>
-          )}          
+          <InnerApp />
         </RecoilRoot>
       </ChakraProvider>
   );
