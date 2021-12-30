674b332f2de148e087c17b5a85d0b0d5aa5cf890
Lord Codelrond <codelrond@protonmail.com>
2021-12-30 16:57:15 +0100

    wip

--- src/components/WorkspaceList.js
+++ src/components/WorkspaceList.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   List,
   ListItem,
-  Box, VStack
+  Box, VStack, Checkbox
 } from "@chakra-ui/react";
 import WorkspaceCard from './WorkspaceCard';
 import { useAllDocs } from 'use-pouchdb';
 
 const WorkspaceList = () => {
+    const [filterTemplates, setFilterTemplates] = useState(false);
     const { rows: workspaces } = useAllDocs({
         db: "localWorkspaces",
         include_docs: true, 
@@ -18,12 +19,21 @@ const WorkspaceList = () => {
             {id: w.id, description: w.doc.description, containers: w.doc.containers, specification: w.doc.specification}
         )
     )
-    console.log(myWorkspaces);
+    
+    function filter(lista){
+        if(filterTemplates){
+            lista.filter(x=>x.isTemplate)
+        }else{
+            return lista
+        }
+    }
+
     return (
         <Box>
             <VStack>
+                <Checkbox isChecked={filterTemplates} onChange={()=>setFilterTemplates(!filterTemplates)} >View only templates</Checkbox>
                 <List spacing={3}>
-                    {myWorkspaces.map(w => 
+                    {filter(myWorkspaces).map(w => 
                         <ListItem key={w.id}>
                             <WorkspaceCard workspace={w} />
                         </ListItem>
